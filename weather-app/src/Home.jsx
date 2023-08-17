5afefa260942382ff6f38e299dcbdad94fc7e66d
Younes <younes.bns@outlook.com>
2023-08-16 22:35:52 -0400

    I added the apiKey I got the weather updating on the screen

--- weather-app/src/Home.jsx
+++ weather-app/src/Home.jsx
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 import sunnyImages from "./assets/images/sunny.png";
+import windIcon from "./assets/images/wind.png";
+import humidityIcon from "./assets/images/humidity.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faEnvelope,
@@ -7,6 +11,33 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import "./Home-css.css";
 function Home() {
+  const [data, setData] = useState({
+    celcius: 10,
+    name: "London",
+    humidity: 10,
+    speed: 2,
+    tempMin: 10,
+    tempMax: 13,
+  });
+
+  useEffect(() => {
+    const apiUrl =
+      "https://api.openweathermap.org/data/2.5/weather?q=London&appid=9be6ef8235949b17a36522f359a10979&units=metric";
+    axios
+      .get(apiUrl)
+      .then((res) => {
+        setData({
+          ...data,
+          celcius: res.data.main.temp,
+          name: res.data.name,
+          humidity: res.data.main.humidity,
+          speed: res.data.wind.speed,
+          tempMax: res.data.main.temp_max,
+          tempMin: res.data.main.temp_min,
+        });
+      })
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <div className="container">
       <h1>weather app</h1>
@@ -18,11 +49,27 @@ function Home() {
           </button>
         </div>
         <div className="weather-info">
-          <h1>22</h1>
-          <h2>Toronto</h2>
+          <div className="city-temp">
+            <img className="city-temp-icon" src={sunnyImages} alt="sunny" />
+            <h1>{data.celcius}°C</h1>
+            <h2>{data.name}</h2>
+          </div>
           <div className="details">
             <div className="col">
-              <img src={sunnyImages} alt="" />
+              <img src={humidityIcon} alt="humidity" />
+              <div>
+                <p>{data.humidity}%</p>
+                <p>Humidity</p>
+              </div>
+            </div>
+            <div className="col">
+              <img src={windIcon} alt="wind" />
+              <p>{data.wind}</p>
+            </div>
+            <div className="col">
+              <p>
+                Min:{data.tempMin}°C || Max: {data.tempMax}°C;
+              </p>
             </div>
           </div>
         </div>
